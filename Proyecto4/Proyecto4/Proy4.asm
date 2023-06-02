68dc1e65b2c6d5b40b045316e7d52cb5ee7d7663
LeivaDiego <lei21752@uvg.edu.gt>
2023-06-02 00:52:52 -0600

    Logica terminada
    Logica del funcionamiento terminado

--- Proyecto4/Proyecto4/Proy4.asm
+++ Proyecto4/Proyecto4/Proy4.asm
@@ -1,14 +1,14 @@
-;|----------------------------------------------------------------------|
+ï»¿;|----------------------------------------------------------------------|
 ;| Universidad del Valle de Guatemala                                   |
-;| José Pablo Orellana - 21970 | Diego Alberto Leiva - 21752            | 
-;| Proyecto 04 - Organización de Computadoras y Assembler               |
+;| JosÃ© Pablo Orellana - 21970 | Diego Alberto Leiva - 21752            | 
+;| Proyecto 04 - OrganizaciÃ³n de Computadoras y Assembler               |
 ;|----------------------------------------------------------------------|
-;| Acertijo Pastor: Consiste en el diseño de un juego de acertijos      |
+;| Acertijo Pastor: Consiste en el diseÃ±o de un juego de acertijos      |
 ;| en el que un pastor tiene que atravesar a la otra orilla de un       |
-;| río con un lobo, una cabra y una lechuga. Dispone de una barca       |
-;| en la que solo caben él y una de las otras tres cosas. Si el lobo    |
+;| rÃ­o con un lobo, una cabra y una lechuga. Dispone de una barca       |
+;| en la que solo caben Ã©l y una de las otras tres cosas. Si el lobo    |
 ;| se queda solo con la cabra se la come, si la cabra se queda sola con | 
-;| la lechuga se la come. ¿Cómo debe hacerlo?                           |
+;| la lechuga se la come. Â¿CÃ³mo debe hacerlo?                           |
 ;|----------------------------------------------------------------------|
 
 
@@ -108,7 +108,7 @@ aloneopt        BYTE    "|                                     4) Regresar Solo 
 
 
 message12		BYTE	"|                   Ingresa el numero 1 para iniciar con el juego:                  |", 0
-errormsg1		BYTE	"|                        Ingresaste un número que no es valido                      |", 0Ah, 0
+errormsg1		BYTE	"|                        Ingresaste un nÃºmero que no es valido                      |", 0Ah, 0
 errormsg2		BYTE	"|                      El objeto no esta disponible de este lado                    |", 0Ah, 0
 pru     		BYTE	"                                       Prueba                                        ", 0Ah, 0
 izwolf       	BYTE	"|                              Lobo esta del lado Izquierdo                         |", 0Ah, 0
@@ -129,10 +129,10 @@ dato1   db "%d", 0
 Inicio1 dd 0
 movida dd 0
 
-wolf    DWORD   1 ;lado del lobo
-sheep   DWORD   1 ;lado de la oveja
-lettuce DWORD   1 ;lado de la lechuga
-pastor  DWORD   1 ;lado del pastop
+wolf    DWORD   0 ;lado del lobo
+sheep   DWORD   0 ;lado de la oveja
+lettuce DWORD   0 ;lado de la lechuga
+pastor  DWORD   0 ;lado del pastop
 
 game    DWORD   2 ;ya ganamos?
 
@@ -186,17 +186,17 @@ main proc
     invoke printf, addr space
     invoke printf, addr separator
 
-    call winlose
-
     call inRangeStart
     mov Inicio1, eax
 
 	.IF Inicio1 == 1 
 		;Programar Juego
-        call actualstate
+    continue:
         call wherePastor
-        
-
+        .IF game == 2
+            jmp continue
+        .ENDIF
+            
 
 	.ELSEIF Inicio1 == 2 
 		;Instrucciones
@@ -211,17 +211,14 @@ main proc
         invoke printf, addr instructions6
         invoke printf, addr instructions7
         invoke printf, addr space
-        invoke printf, addr wolfopt
-        invoke printf, addr sheepopt
-        invoke printf, addr lettuceopt
-        invoke printf, addr space
         invoke printf, addr separator
+        ;pedir cualquier cosa aca
+        jmp continue
 
 
 
 	.ELSEIF Inicio1 == 3   
 		;Salir
-        invoke printf, addr space
         invoke printf, addr separator
         invoke printf, addr finish
         invoke printf, addr separator
@@ -247,6 +244,10 @@ main endp
 dummy proc
     ret
 dummy endp
+
+dummy2 proc
+    ret
+dummy2 endp
 
 ;-----------------------------------------------------------------------------------------------------------------------
 
@@ -286,14 +287,14 @@ winer endp
 inRangeStart proc                   ;Creador: Diego Leiva Valida si un numero esta entre 1,2 o 3 y sino lo vuleve a pedir
     asknum:                         ; Define la etiqueta "asknum" (inicio del bucle)
     sub esp, 4                      ; Reduce la pila en 4 bytes para alojar el valor ingresado.
-    push offset select              ; Empuja la dirección del mensaje "select" a la pila para printf.
-    call printf                     ; Llama a la función printf para imprimir el mensaje de solicitud.
-    add esp, 8                      ; Aumenta la pila en 8 bytes, limpiando la dirección del mensaje y el valor anterior.
+    push offset select              ; Empuja la direcciÃ³n del mensaje "select" a la pila para printf.
+    call printf                     ; Llama a la funciÃ³n printf para imprimir el mensaje de solicitud.
+    add esp, 8                      ; Aumenta la pila en 8 bytes, limpiando la direcciÃ³n del mensaje y el valor anterior.
 
-    lea eax, [ebp - 4]              ; Carga la dirección de la variable local en eax.
-    push eax                        ; Empuja la dirección de la variable local a la pila para scanf.
-    push offset dato1               ; Empuja la dirección del formato de entrada a la pila para scanf.
-    call scanf                      ; Llama a la función scanf para leer el valor de entrada.
+    lea eax, [ebp - 4]              ; Carga la direcciÃ³n de la variable local en eax.
+    push eax                        ; Empuja la direcciÃ³n de la variable local a la pila para scanf.
+    push offset dato1               ; Empuja la direcciÃ³n del formato de entrada a la pila para scanf.
+    call scanf                      ; Llama a la funciÃ³n scanf para leer el valor de entrada.
     add esp, 8                      ; Aumenta la pila en 8 bytes, limpiando las direcciones de la pila.
 
     mov eax, [ebp - 4]              ; Mueve el valor de entrada a eax.
@@ -310,9 +311,9 @@ inRangeStart proc                   ;Creador: Diego Leiva Valida si un numero es
     invoke printf, addr errormsg1   ; Si no es igual a ninguno, imprime el mensaje de error.
     jmp asknum                      ; Y luego salta a "asknum" para pedir otro valor.
 
-    valid:                          ; Define la etiqueta "valid" (salto si el valor de entrada es válido).
+    valid:                          ; Define la etiqueta "valid" (salto si el valor de entrada es vÃ¡lido).
     mov Inicio1, eax                ; Guarda el valor de entrada en Inicio1.
-    ret                             ; Regresa al código que llamó a esta subrutina.
+    ret                             ; Regresa al cÃ³digo que llamÃ³ a esta subrutina.
 
 inRangeStart endp
 
@@ -390,22 +391,40 @@ actualstate endp
 
 ;-----------------------------------------------------------------------------------------------------------------------
 
-wherePastor proc
+wherePastor proc                                ;Creador Diego Leiva Evalua en que lado se encuentra el pastor, le pide que cosa quiere mover y evalua si gano o perdio el juego
     .IF pastor == 0 
 		;el pastor esta en la izquierda
+        call actualstate
         call leftOptions
         call questionleft
-        
-
+        call movidasleft
+        call winlose
+        .IF game == 0
+            jmp endl
+        .ENDIF
+        .IF game == 1
+            jmp endl
+        .ENDIF
     .ENDIF
-
+    
 	.IF pastor == 1
         ;el pastor esta en la derecha
+        call actualstate
         call rightOptions
         call questionright
-        
+        call movidasright
+        call winlose
+        .IF game == 0
+            jmp endl
+        .ENDIF
+        .IF game == 1
+            jmp endl
+        .ENDIF
 	.ENDIF
-    ret
+
+
+    endl:
+      ret
 wherePastor endp
 
 ;-----------------------------------------------------------------------------------------------------------------------
@@ -454,18 +473,18 @@ rightOptions endp
 
 ;-----------------------------------------------------------------------------------------------------------------------
 
-questionleft proc
+questionleft proc                   ;Creador   Pablo Orellana Pide al jugador que objeto mover de izquierda a derecha e indica si la opcion es valida o no
     askopt:
 
     sub esp, 4                      ; Reduce la pila en 4 bytes para alojar el valor ingresado.
-    push offset select              ; Empuja la dirección del mensaje "select" a la pila para printf.
-    call printf                     ; Llama a la función printf para imprimir el mensaje de solicitud.
-    add esp, 8                      ; Aumenta la pila en 8 bytes, limpiando la dirección del mensaje y el valor anterior.
+    push offset select              ; Empuja la direcciÃ³n del mensaje "select" a la pila para printf.
+    call printf                     ; Llama a la funciÃ³n printf para imprimir el mensaje de solicitud.
+    add esp, 8                      ; Aumenta la pila en 8 bytes, limpiando la direcciÃ³n del mensaje y el valor anterior.
 
-    lea eax, [ebp - 4]              ; Carga la dirección de la variable local en eax.
-    push eax                        ; Empuja la dirección de la variable local a la pila para scanf.
-    push offset dato1               ; Empuja la dirección del formato de entrada a la pila para scanf.
-    call scanf                      ; Llama a la función scanf para leer el valor de entrada.
+    lea eax, [ebp - 4]              ; Carga la direcciÃ³n de la variable local en eax.
+    push eax                        ; Empuja la direcciÃ³n de la variable local a la pila para scanf.
+    push offset dato1               ; Empuja la direcciÃ³n del formato de entrada a la pila para scanf.
+    call scanf                      ; Llama a la funciÃ³n scanf para leer el valor de entrada.
     add esp, 8                      ; Aumenta la pila en 8 bytes, limpiando las direcciones de la pila.
 
     mov eax, [ebp - 4]              ; Mueve el valor de entrada a eax.
@@ -506,27 +525,27 @@ questionleft proc
     invoke printf, addr errormsg1   ; Si no es igual a ninguno, imprime el mensaje de error.
     jmp askopt                      ; Y luego salta a "asknum" para pedir otro valor.
 
-    valid:                          ; Define la etiqueta "valid" (salto si el valor de entrada es válido).
+    valid:                          ; Define la etiqueta "valid" (salto si el valor de entrada es vÃ¡lido).
     mov movida, eax                ; Guarda el valor de entrada en Inicio1.
-    ret                             ; Regresa al código que llamó a esta subrutina.
+    ret                             ; Regresa al cÃ³digo que llamÃ³ a esta subrutina.
 
     ret
 questionleft endp
 
 ;-----------------------------------------------------------------------------------------------------------------------
 
-questionright proc
+questionright proc                  ;Creador   Pablo Orellana Pide al jugador que objeto mover de derecha a izquierda e indica si la opcion es valida o no
     askopt:
 
     sub esp, 4                      ; Reduce la pila en 4 bytes para alojar el valor ingresado.
-    push offset select              ; Empuja la dirección del mensaje "select" a la pila para printf.
-    call printf                     ; Llama a la función printf para imprimir el mensaje de solicitud.
-    add esp, 8                      ; Aumenta la pila en 8 bytes, limpiando la dirección del mensaje y el valor anterior.
+    push offset select              ; Empuja la direcciÃ³n del mensaje "select" a la pila para printf.
+    call printf                     ; Llama a la funciÃ³n printf para imprimir el mensaje de solicitud.
+    add esp, 8                      ; Aumenta la pila en 8 bytes, limpiando la direcciÃ³n del mensaje y el valor anterior.
 
-    lea eax, [ebp - 4]              ; Carga la dirección de la variable local en eax.
-    push eax                        ; Empuja la dirección de la variable local a la pila para scanf.
-    push offset dato1               ; Empuja la dirección del formato de entrada a la pila para scanf.
-    call scanf                      ; Llama a la función scanf para leer el valor de entrada.
+    lea eax, [ebp - 4]              ; Carga la direcciÃ³n de la variable local en eax.
+    push eax                        ; Empuja la direcciÃ³n de la variable local a la pila para scanf.
+    push offset dato1               ; Empuja la direcciÃ³n del formato de entrada a la pila para scanf.
+    call scanf                      ; Llama a la funciÃ³n scanf para leer el valor de entrada.
     add esp, 8                      ; Aumenta la pila en 8 bytes, limpiando las direcciones de la pila.
 
     mov eax, [ebp - 4]              ; Mueve el valor de entrada a eax.
@@ -570,11 +589,10 @@ questionright proc
     invoke printf, addr errormsg1   ; Si no es igual a ninguno, imprime el mensaje de error.
     jmp askopt                      ; Y luego salta a "asknum" para pedir otro valor.
 
-    valid:                          ; Define la etiqueta "valid" (salto si el valor de entrada es válido).
+    valid:                          ; Define la etiqueta "valid" (salto si el valor de entrada es vÃ¡lido).
     mov movida, eax                ; Guarda el valor de entrada en Inicio1.
-    ret                             ; Regresa al código que llamó a esta subrutina.
+    ret                             ; Regresa al cÃ³digo que llamÃ³ a esta subrutina.
 
-    ret
 questionright endp
 
 ;-----------------------------------------------------------------------------------------------------------------------
@@ -625,7 +643,42 @@ winlose proc                    ;Creador Diego Leiva Subrutina que berifica si e
             .ENDIF
         .ENDIF 
     .ENDIF
-
+    
+    ret
 winlose endp
+
+;-----------------------------------------------------------------------------------------------------------------------
+
+movidasleft proc            ;Creador Diego Leiva Movida del pastor con objeto a la derecha
+    .IF movida == 1
+        mov wolf, 1
+    .ENDIF
+    .IF movida == 2
+        mov sheep, 1
+    .ENDIF
+    .IF movida == 3
+        mov lettuce, 1
+    .ENDIF
+    mov pastor, 1
+    ret
+movidasleft endp
+
+;-----------------------------------------------------------------------------------------------------------------------
+
+movidasright proc           ;Creador Diego Leiva Movida del pastor con o sin objeto a la izquierda
+    .IF movida == 1
+        mov wolf, 0
+    .ENDIF
+    .IF movida == 2
+        mov sheep, 0
+    .ENDIF
+    .IF movida == 3
+        mov lettuce, 0
+    .ENDIF
+    mov pastor, 0
+    ret
+movidasright endp
+
+
 
 end
